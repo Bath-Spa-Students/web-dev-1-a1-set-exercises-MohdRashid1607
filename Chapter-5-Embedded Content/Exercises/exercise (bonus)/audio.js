481a9f994d72afc4f79254592381677a9e027ce5
lmfmfombeo <mr03539@gmail.com>
2024-03-29 11:17:06 -0700

    Update audio.js

--- Chapter-5-Embedded Content/Exercises/exercise (bonus)/audio.js
+++ Chapter-5-Embedded Content/Exercises/exercise (bonus)/audio.js
@@ -1,18 +1,7 @@
-function calc(){ //creating a function called "calc"
-
-    //retrives values from the input field from the html file with the matching id and stores them in the respective variables.
-    //parseFloat converts the datatype to Float
-    var petrol_cost = parseFloat(document.getElementById('Petrolcost').value); 
-    var  total_liters = parseFloat(document.getElementById('Liters').value);
-    
-    //to check if the values inserted are numbers by using "isNan"
-    if(isNaN(petrol_cost) || isNaN(total_liters)){
-        
-        //if they arent a number then the output will be "Invalid input"
-        document.querySelector('#totalcost').innerText = "Invalid input";
-    } else {
-
-        //if the requirment is satisfed the "petrol_cost" variable is multipled with the "total_liters" variable
-        document.querySelector('#totalcost').innerText = "£" + petrol_cost * total_liters;
-    }
+function playAudio(clicked_id) {
+    //call audio
+    var audio = new Audio("Audio Sampler/Audio/" + clicked_id + ".mp3"); /* "Audio Sampler/Audio/" = location of audios
+                                                                            clicked_id = id of the button
+                                                                            ".mp3" = audio file extension string */
+    audio.play(); //play audio
 }
