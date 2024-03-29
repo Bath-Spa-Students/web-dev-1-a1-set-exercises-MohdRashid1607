72e6de25f986c9b57b0c3aae3098c23a05b835f3
lmfmfombeo <mr03539@gmail.com>
2024-03-29 12:02:29 -0700

    Update audio.js

--- Chapter-5-Embedded Content/Exercises/exercise (bonus)/audio.js
+++ Chapter-5-Embedded Content/Exercises/exercise (bonus)/audio.js
@@ -1,15 +1,48 @@
-function playSoundEffect(audioFileName) {
-    var audio = new Audio('audio/' + audioFileName);
+//creating function
+function ahha() {
+    var audioSrc = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/ah-ha.mp3"; //audio source
+    var audio = new Audio(audioSrc);
+    audio.play(); //playing audio
+}
+
+function botn() {
+    var audioSrc = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/back-of-the-net.mp3";
+    var audio = new Audio(audioSrc);
     audio.play();
 }
 
-function textToAudio() {
-    let msg = document.getElementById("text-to-speech").value;
-    let speech = new SpeechSynthesisUtterance();
-    speech.lang = "en-US";
-    speech.text = msg;
-    speech.volume = 1;
-    speech.rate = 1;
-    speech.pitch = 1;
-    window.speechSynthesis.speak(speech);
+function booo() {
+    var audioSrc = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/bangoutoforder.mp3";
+    var audio = new Audio(audioSrc);
+    audio.play();
+}
+
+function dan() {
+    var audioSrc = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/dan.mp3";
+    var audio = new Audio(audioSrc);
+    audio.play();
+}
+
+function eote() {
+    var audioSrc = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/emailoftheevening.mp3";
+    var audio = new Audio(audioSrc);
+    audio.play();
+}
+
+function hp() {
+    var audioSrc = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/hellopartridge.mp3";
+    var audio = new Audio(audioSrc);
+    audio.play();
+}
+
+function iase() {
+    var audioSrc = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/iateascotchegg.mp3";
+    var audio = new Audio(audioSrc);
+    audio.play();
+}
+
+function imc() {
+    var audioSrc = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/imconfused.mp3";
+    var audio = new Audio(audioSrc);
+    audio.play();
 }
