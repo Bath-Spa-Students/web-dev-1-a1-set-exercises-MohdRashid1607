801d426913f2a7c3672ecd8817fe6c755e69935b
lmfmfombeo <mr03539@gmail.com>
2024-03-30 04:21:32 -0700

    Update audio.js

---
+++ Chapter-5-Embedded Content/Exercises/exercise (bonus)/audio.js
@@ -0,0 +1,6 @@
+//creating function
+function ahha() {
+    var audioSrc = "/Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/ah-ha.mp3"; //audio source
+    var audio = new Audio(audioSrc);
+    audio.play(); //playing audio 
+}
